63bd3586ded63fcbfae6596ab7cd9aee12fc9bff
tophat8855 <hrfarley@gmail.com>
2014-11-19 13:09:22 -0800

    all controllers have working views and vice versa

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -12,10 +12,27 @@ class CupcakesController < ApplicationController
   end
 
   def maple_bacon
-
+    @sizes = [
+      'mini',
+      'regular',
+      'jumbo',
+      'actual cake',
+    ]
   end
 
   def red_velvet
-
+    @ingredients = {
+      flour: '1 tbsp + 2 tsp',
+      cocoa: '1/4 tsp',
+      salt: 'pinchette',
+      sugar: '1 tbsp',
+      oil: '1 tbsp',
+      egg: '3/4 tsp',
+      red_food_coloring: '11 drops',
+      vanilla: '1/16 tsp',
+      buttermilk: '2 tsp',
+      baking_soda: '1/16 tsp',
+      vinegar: '1/8 tsp',
+    }
   end
 end
